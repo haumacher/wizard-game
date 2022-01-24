dddf1901f0b47121f3a204e95c70ddc2acc3de69
Bernhard Haumacher <haui@haumacher.de>
2022-01-24 18:21:22 +0100

    Only display trump selection cards for the player which should select.

--- client/src/main/java/de/haumacher/wizard/ui/TrumpSelection.java
+++ client/src/main/java/de/haumacher/wizard/ui/TrumpSelection.java
@@ -3,7 +3,7 @@
  */
 package de.haumacher.wizard.ui;
 
-import de.haumacher.wizard.controller.Controller;
+import de.haumacher.wizard.controller.GenericController;
 import de.haumacher.wizard.io.WizardConnection;
 import de.haumacher.wizard.msg.Card;
 import de.haumacher.wizard.msg.Player;
@@ -16,11 +16,9 @@ import javafx.scene.layout.TilePane;
 import javafx.scene.text.Text;
 
 /**
- * TODO
- *
- * @author <a href="mailto:haui@haumacher.de">Bernhard Haumacher</a>
+ * View that is displayed if a wizard was chosen as "trump" card to allow a player to select the trump color. 
  */
-public class TrumpSelection extends Controller {
+public class TrumpSelection extends GenericController {
 	
 	@FXML
 	TilePane selectPane;
@@ -28,32 +26,22 @@ public class TrumpSelection extends Controller {
 	@FXML
 	Text info;
 
-	private WizardConnection _server;
-
-	private Player _player;
-
-	private boolean _allowSelect;
-
 	public void init(WizardConnection server, Player player, boolean allowSelect) {
-		_server = server;
-		_player = player;
-		_allowSelect = allowSelect;
-		
-		if (_allowSelect) {
+		if (allowSelect) {
 			info.setText("Du darfst die Trumpffarbe wählen!");
+			
+			for (Suit suit : Suit.values()) {
+				Node cardView = CardView.createCard(Card.create().setSuit(suit).setValue(Value.Z));
+				cardView.setOnMouseClicked(e -> {
+					server.sendCommand(SelectTrump.create().setTrumpSuit(suit));
+				});
+				
+				selectPane.getChildren().add(cardView);
+			}
 		} else {
 			info.setText(player.getName() + " wählt die Trumpffarbe...");
-		}
-		
-		for (Suit suit : Suit.values()) {
-			Node cardView = CardView.createCard(Card.create().setSuit(suit).setValue(Value.Z));
-			selectPane.getChildren().add(cardView);
-
-			if (allowSelect) {
-				cardView.setOnMouseClicked(e -> {
-					_server.sendCommand(SelectTrump.create().setTrumpSuit(suit));
-				});
-			}
+			
+			selectPane.getChildren().clear();
 		}
 	}
 
